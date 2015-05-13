2453c3c46d81c9c4c6087fe18a36e4e0eedadae1
NKsjc <1624285520@qq.com>
2015-05-13 22:28:21 +0800

    version1.1
    完成方格样式的一系列操作

--- Tetris/Tetris/Form1.Designer.cs
+++ Tetris/Tetris/Form1.Designer.cs
@@ -31,7 +31,16 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.labelModel = new System.Windows.Forms.Label();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lsvBlockSet = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblColor = new System.Windows.Forms.Label();
+            this.lblModel = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.SuspendLayout();
@@ -45,7 +54,7 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(284, 192);
+            this.tabControl1.Size = new System.Drawing.Size(465, 302);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -55,37 +64,122 @@
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.tabPage1.Size = new System.Drawing.Size(276, 166);
+            this.tabPage1.Size = new System.Drawing.Size(457, 251);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "参数配置";
             // 
             // tabPage2
             // 
             this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
-            this.tabPage2.Controls.Add(this.labelModel);
+            this.tabPage2.Controls.Add(this.btnUpdate);
+            this.tabPage2.Controls.Add(this.btnClear);
+            this.tabPage2.Controls.Add(this.btnDel);
+            this.tabPage2.Controls.Add(this.btnAdd);
+            this.tabPage2.Controls.Add(this.lsvBlockSet);
+            this.tabPage2.Controls.Add(this.lblColor);
+            this.tabPage2.Controls.Add(this.lblModel);
             this.tabPage2.ForeColor = System.Drawing.SystemColors.Control;
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(276, 166);
+            this.tabPage2.Size = new System.Drawing.Size(457, 276);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "砖块样式配置";
             // 
-            // labelModel
+            // btnDel
             // 
-            this.labelModel.Location = new System.Drawing.Point(3, 0);
-            this.labelModel.Name = "labelModel";
-            this.labelModel.Size = new System.Drawing.Size(156, 156);
-            this.labelModel.TabIndex = 0;
-            this.labelModel.Text = "label1";
-            this.labelModel.Paint += new System.Windows.Forms.PaintEventHandler(this.labelModel_Paint);
-            this.labelModel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.labelModel_MouseClick);
+            this.btnDel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnDel.Location = new System.Drawing.Point(99, 221);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 5;
+            this.btnDel.Text = "删除";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAdd.Location = new System.Drawing.Point(6, 221);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "添加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lsvBlockSet
+            // 
+            this.lsvBlockSet.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lsvBlockSet.FullRowSelect = true;
+            this.lsvBlockSet.GridLines = true;
+            this.lsvBlockSet.Location = new System.Drawing.Point(180, 3);
+            this.lsvBlockSet.MultiSelect = false;
+            this.lsvBlockSet.Name = "lsvBlockSet";
+            this.lsvBlockSet.Size = new System.Drawing.Size(274, 242);
+            this.lsvBlockSet.TabIndex = 2;
+            this.lsvBlockSet.UseCompatibleStateImageBehavior = false;
+            this.lsvBlockSet.View = System.Windows.Forms.View.Details;
+            this.lsvBlockSet.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.lsvBlockSet_ItemSelectionChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "编码";
+            this.columnHeader1.Width = 147;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "颜色";
+            this.columnHeader2.Width = 124;
+            // 
+            // lblColor
+            // 
+            this.lblColor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblColor.Location = new System.Drawing.Point(3, 185);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(150, 23);
+            this.lblColor.TabIndex = 1;
+            this.lblColor.Click += new System.EventHandler(this.lblColor_Click);
+            // 
+            // lblModel
+            // 
+            this.lblModel.Location = new System.Drawing.Point(3, 0);
+            this.lblModel.Name = "lblModel";
+            this.lblModel.Size = new System.Drawing.Size(156, 156);
+            this.lblModel.TabIndex = 0;
+            this.lblModel.Text = "label1";
+            this.lblModel.Paint += new System.Windows.Forms.PaintEventHandler(this.labelModel_Paint);
+            this.lblModel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lblModel_MouseClick);
+            // 
+            // btnClear
+            // 
+            this.btnClear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnClear.Location = new System.Drawing.Point(99, 251);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "清空";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnUpdate.Location = new System.Drawing.Point(8, 251);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "修改";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // frmConfigure
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(465, 303);
             this.Controls.Add(this.tabControl1);
             this.Name = "frmConfigure";
             this.Text = "属性配置";
@@ -100,7 +194,16 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.Label labelModel;
+        private System.Windows.Forms.Label lblModel;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListView lsvBlockSet;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnUpdate;
     }
 }
 
--- Tetris/Tetris/Form1.cs
+++ Tetris/Tetris/Form1.cs
@@ -31,6 +31,7 @@ namespace Tetris
             gp.Clear(Color.Black);
             Pen pDraw = new Pen(Color.White);
             Point ps, pe;
+            SolidBrush sb = new SolidBrush(blockColor );
             for (int i = 31; i < 155; i=i+31)
             {
                 ps=new Point(1,i);
@@ -43,7 +44,19 @@ namespace Tetris
                 pe = new Point(i, 155);
                 gp.DrawLine(pDraw, ps, pe);
             }
-            gp.Dispose();//释放gp
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (strArr[i,j]==true)
+                    {
+                        gp.FillRectangle(sb, 31 * i + 1, 31 * j + 1, 30, 30);
+                    }
+                }
+                
+            }
+            //在这不能释放gp......
+            //gp.Dispose();//释放gp
         }
         /// <summary>
         /// 2015.03.13
@@ -53,7 +66,7 @@ namespace Tetris
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void labelModel_MouseClick(object sender, MouseEventArgs e)
+        private void lblModel_MouseClick(object sender, MouseEventArgs e)
         {
             if (e.Button!=MouseButtons.Left)//判断是左键or右键
             {
@@ -64,10 +77,147 @@ namespace Tetris
             yPos = e.Y / 31;
             strArr[xPos, yPos] = !strArr[xPos, yPos];
             bool change = strArr[xPos, yPos];
-            Graphics gp = labelModel.CreateGraphics();
+            Graphics gp = lblModel.CreateGraphics();
             SolidBrush sb = new SolidBrush(change?blockColor:Color.Black);
             gp.FillRectangle(sb,31*xPos+1,31*yPos+1,30,30);
             gp.Dispose();//释放gp
         }
+
+        private void lblColor_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            blockColor = colorDialog1.Color;
+            lblColor.BackColor = blockColor;
+            lblModel.Invalidate();
+        }
+        /// <summary>
+        /// 2015.03.13
+        /// 添加设置的砖块格式，显示在lsvBlock中
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            bool isEmpty = false;
+            foreach (var item in strArr)
+            {
+                if (item==true)
+                {
+                    isEmpty = true;
+                    break;
+
+                }
+            }
+            if (isEmpty==false)
+            {
+                MessageBox.Show("图像为空，请绘制图像","提示窗口");
+                return;
+            }
+            StringBuilder strBuilder = new StringBuilder(25);
+            foreach (var item in strArr)
+            {
+                strBuilder.Append(item?"1":"0");
+            }
+            string str = strBuilder.ToString();
+            foreach (ListViewItem item in lsvBlockSet.Items)
+            {
+                if (item.SubItems[0].Text==str)
+                {
+                    MessageBox.Show("图案已经存在", "提示窗口");
+                    return;
+                }
+            }
+            ListViewItem lvi = new ListViewItem();
+            lvi = lsvBlockSet.Items.Add(str);
+            lvi.SubItems.Add(blockColor.ToArgb().ToString());
+        }
+        /// <summary>
+        /// 2015.03.13
+        /// 选择lsvBlockSet中的不同样式，显示出来
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lsvBlockSet_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            if (e.IsSelected)
+            {
+                blockColor = Color.FromArgb(int.Parse(e.Item.SubItems[1].Text));
+                string str = e.Item.SubItems[0].Text.ToString();
+                for (int i = 0; i < 25; i++)
+                {
+                    strArr[i / 5, i % 5] =(str[i]=='1')?true:false;
+                }
+                lblModel.Invalidate();
+            }
+        }
+        /// <summary>
+        /// 2015.03.13
+        /// 删除列表中选中的样式，并且在图案框中清空
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (lsvBlockSet.SelectedItems.Count==0)
+            {
+                MessageBox.Show("请选择一个项目");
+                return;
+            }
+            lsvBlockSet.Items.Remove(lsvBlockSet.SelectedItems[0]);
+            btnClear.PerformClick();
+        }
+        /// <summary>
+        /// 清空图案框中的内容
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    strArr[i,j] = false;
+                }
+            }
+            lblModel.Invalidate();
+        }
+        /// <summary>
+        /// 修改列表中的图案样式
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            bool isEmpty = false;
+            foreach (var item in strArr)
+            {
+                if (item == true)
+                {
+                    isEmpty = true;
+                    break;
+                }
+            }
+            if (isEmpty == false)
+            {
+                MessageBox.Show("图像为空，请绘制图像", "提示窗口");
+                return;
+            }
+            if (lsvBlockSet.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("请选择一个项目");
+                return;
+            }
+            StringBuilder strBuilder = new StringBuilder(25);
+            foreach (var item in strArr)
+            {
+                strBuilder.Append(item?"1":"0");
+            }
+            lsvBlockSet.SelectedItems[0].SubItems[0].Text = strBuilder.ToString();
+            lsvBlockSet.SelectedItems[0].SubItems[1].Text = Convert.ToString(blockColor.ToArgb());
+        }
+
+      
     }
 }
