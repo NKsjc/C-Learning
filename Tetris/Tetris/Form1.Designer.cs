9329d8e707bbfda874e55a9d4c28bc41eac910cd
NKsjc <1624285520@qq.com>
2015-05-13 21:04:06 +0800

    俄罗斯方块1.0

---
+++ Tetris/Tetris/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Tetris
+{
+    partial class frmConfigure
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.labelModel = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)), true);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(284, 192);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tabPage1.Size = new System.Drawing.Size(276, 166);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "参数配置";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Controls.Add(this.labelModel);
+            this.tabPage2.ForeColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(276, 166);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "砖块样式配置";
+            // 
+            // labelModel
+            // 
+            this.labelModel.Location = new System.Drawing.Point(3, 0);
+            this.labelModel.Name = "labelModel";
+            this.labelModel.Size = new System.Drawing.Size(156, 156);
+            this.labelModel.TabIndex = 0;
+            this.labelModel.Text = "label1";
+            this.labelModel.Paint += new System.Windows.Forms.PaintEventHandler(this.labelModel_Paint);
+            this.labelModel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.labelModel_MouseClick);
+            // 
+            // frmConfigure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "frmConfigure";
+            this.Text = "属性配置";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label labelModel;
+    }
+}
+
---
+++ Tetris/Tetris/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    public partial class frmConfigure : Form
+    {
+        public frmConfigure()
+        {
+            InitializeComponent();
+        }
+
+        private bool[,] strArr = new bool[5, 5];
+        private Color blockColor = Color.Red;
+
+        /// <summary>
+        /// 2015.05.13，完成砖块的模型
+        /// 画出一个5*5的方格
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labelModel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gp = e.Graphics;
+            gp.Clear(Color.Black);
+            Pen pDraw = new Pen(Color.White);
+            Point ps, pe;
+            for (int i = 31; i < 155; i=i+31)
+            {
+                ps=new Point(1,i);
+                pe=new Point(155,i);
+                gp.DrawLine(pDraw,ps,pe);
+            }
+            for (int i = 31; i < 155; i = i + 31)
+            {
+                ps = new Point(i, 1);
+                pe = new Point(i, 155);
+                gp.DrawLine(pDraw, ps, pe);
+            }
+            gp.Dispose();//释放gp
+        }
+        /// <summary>
+        /// 2015.03.13
+        /// 方格点击事件，判断点击方块的反应事件
+        /// strArr是bool型数组，记录方格的值来对应方格的颜色
+        /// 初始化时值都为false
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labelModel_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button!=MouseButtons.Left)//判断是左键or右键
+            {
+                return;
+            }
+            int xPos, yPos;
+            xPos = e.X / 31;
+            yPos = e.Y / 31;
+            strArr[xPos, yPos] = !strArr[xPos, yPos];
+            bool change = strArr[xPos, yPos];
+            Graphics gp = labelModel.CreateGraphics();
+            SolidBrush sb = new SolidBrush(change?blockColor:Color.Black);
+            gp.FillRectangle(sb,31*xPos+1,31*yPos+1,30,30);
+            gp.Dispose();//释放gp
+        }
+    }
+}
